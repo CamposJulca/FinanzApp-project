ebbb640924b8e8bb1bfa49b9e849ab9639635aa8
cristhiamdaniel-cj <crcampos@javeriana.edu.co>
2026-02-07 01:19:40 +0000

    chore(admin): align admin with transaction classification tree

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Household, Account, Category, Transaction
-
+from .models import (
+    Household,
+    Account,
+    Transaction,
+    TransactionType,
+    TransactionCategory,
+    TransactionSubCategory,
+)
 
 @admin.register(Household)
 class HouseholdAdmin(admin.ModelAdmin):
@@ -15,16 +21,36 @@ class AccountAdmin(admin.ModelAdmin):
     search_fields = ("name",)
 
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "category_type", "user")
-    list_filter = ("category_type", "user")
+@admin.register(TransactionType)
+class TransactionTypeAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(TransactionCategory)
+class TransactionCategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "type")
+    list_filter = ("type",)
+    search_fields = ("name",)
+
+
+@admin.register(TransactionSubCategory)
+class TransactionSubCategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "category")
+    list_filter = ("category",)
     search_fields = ("name",)
 
 
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "account", "category", "amount", "created_at")
-    list_filter = ("category__category_type", "account", "user")
+    list_display = (
+        "id",
+        "user",
+        "account",
+        "subcategory",
+        "amount",
+        "created_at",
+    )
+    list_filter = ("subcategory__category__type", "account", "user")
     search_fields = ("description",)
     ordering = ("-created_at",)
